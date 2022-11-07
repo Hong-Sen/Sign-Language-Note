56946aeff1d3b1da50d78f5018abed35ac585857
Hong-Sen <20181053@sungshin.ac.kr>
2022-11-07 11:54:30 +0900

    Initial commit

---
+++ Sign-Language-Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sign-Language-Note
+//
+//  Created by 홍세은 on 2022/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
